0757e256647b3302f090efe8167dba58f17fa9f5
langletamelie <amelie.langlet@hotmail.com>
2018-07-25 14:06:57 +0200

    php1.1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP 1.1</title>
+  </head>
+  <body>
+    <?php
+$name = Lucien;
+echo $name;
+    ?>
+  </body>
+</html>
